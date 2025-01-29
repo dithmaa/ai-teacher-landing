2bd31f1e0561fcdd0db574389c3cccf1528bc474
Baltukov Nomto <disma785@gmail.com>
2025-01-30 04:43:23 +0800

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
+import Slider from "react-slick";
+import Logo from "./assets/logo.png";
+import Testimonial1 from "./assets/slider/1.jpg";
+import threeHundred from "./assets/slider/three-hundred.png";
+import features1 from "./assets/features/1.png";
+import features2 from "./assets/features/2.png";
+import features3 from "./assets/features/3.png";
+import features4 from "./assets/features/4.png";
+import features5 from "./assets/features/5.png";
+import step1 from "./assets/steps/1.png";
+import step2 from "./assets/steps/2.png";
+import step3 from "./assets/steps/3.png";
+import step4 from "./assets/steps/4.png";
+import footerBg from "./assets/footer-bg.png";
+import mainBg from "./assets/main-bg.png";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const settings = {
+  dots: true, // Показать точки навигации
+  infinite: true, // Бесконечный скролл
+  speed: 500, // Скорость перехода
+  slidesToShow: 1, // Количество слайдов на экране
+  slidesToScroll: 1, // Количество слайдов при прокрутке
+};
 
 const App = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col items-center p-6 text-center">
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center p-6 pb-0">
       <header className="w-full max-w-3xl">
-        <img
-          src="logo.png"
-          alt="FitMentor Logo"
-          className="mx-auto w-24 mb-4"
-        />
-        <h1 className="text-3xl font-bold text-gray-900">
-          Приведи себя в лучшую форму своей жизни!
-        </h1>
-        <p className="text-gray-700 mt-2">
-          Преобразуйте фитнес и питание через персональные онлайн-тренировки:
-          чаты с тренером, живые видео и приложение для ваших целей.
-        </p>
-        <button className="mt-4 px-6 py-2 bg-purple-600 text-white rounded-full shadow-lg hover:bg-purple-700">
-          Записаться
-        </button>
+        <img src={Logo} alt="FitMentor Logo" className="w-50 mb-4" />
       </header>
+      <main className="max-w-3xl w-full flex">
+        <div>
+          <h1 className="text-[50px] font-bold text-gray-900">
+            Приведи себя в лучшую форму своей жизни!
+          </h1>
+          <p className="text-gray-700 mt-2">
+            Преобразуйте фитнес и питание через персональные онлайн-тренировки:
+            чаты с тренером, живые видео и приложение для ваших целей.
+          </p>
+          <button className="mt-4 px-6 py-2 bg-purple-600 text-white rounded-full shadow-lg hover:bg-purple-700">
+            Записаться
+          </button>
+        </div>
+        <img src={mainBg} alt="" className="max-w-[400px] object-cover" />
+      </main>
 
       <section className="mt-8 w-full max-w-3xl bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-xl font-bold text-purple-600">
@@ -33,20 +58,32 @@ const App = () => {
         </h3>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-          <div className="flex items-center">
-            <img src="icon1.png" alt="icon" className="w-10 h-10 mr-3" />
+          <div className="flex items-center flex-col">
+            <div className="flex items-center">
+              <span>1</span>
+              <img src={step1} alt="" />
+            </div>
             <p>Пройдите тест и подберите идеального тренера</p>
           </div>
-          <div className="flex items-center">
-            <img src="icon2.png" alt="icon" className="w-10 h-10 mr-3" />
+          <div className="flex items-center flex-col">
+            <div className="flex items-center">
+              <span>2</span>
+              <img src={step2} alt="" />
+            </div>
             <p>Видео-звонок для составления плана тренировок и питания</p>
           </div>
-          <div className="flex items-center">
-            <img src="icon3.png" alt="icon" className="w-10 h-10 mr-3" />
+          <div className="flex items-center flex-col">
+            <div className="flex items-center">
+              <span>3</span>
+              <img src={step3} alt="" />
+            </div>
             <p>Выполняйте тренировки, разработанные специально для вас</p>
           </div>
-          <div className="flex items-center">
-            <img src="icon4.png" alt="icon" className="w-10 h-10 mr-3" />
+          <div className="flex items-center flex-col">
+            <div className="flex items-center">
+              <span>4</span>
+              <img src={step4} alt="" />
+            </div>
             <p>Будьте очень мотивированы в время еженедельных проверок</p>
           </div>
         </div>
@@ -59,34 +96,34 @@ const App = () => {
               <strong>Подберем проверенного тренера</strong> на базе вашего
               запроса
             </p>
-            <img src="icon1.png" alt="icon" className="w-10 h-10" />
+            <img src={features1} alt="icon" className="w-10 h-10" />
           </div>
           <div className="flex justify-between items-center p-4 rounded-lg bg-gray-50">
             <p className="text-purple-600">
               <strong>Гибкое расписание.</strong> Наши тренера работают в онлайн
               режиме 24/7
             </p>
-            <img src="icon1.png" alt="icon" className="w-10 h-10" />
+            <img src={features2} alt="icon" className="w-10 h-10" />
           </div>
           <div className="flex justify-between items-center p-4 rounded-lg bg-gray-50">
             <p>
               <strong>Высокая вовлеченность тренера</strong> и контроль техники
             </p>
-            <img src="icon1.png" alt="icon" className="w-10 h-10" />
+            <img src={features3} alt="icon" className="w-10 h-10" />
           </div>
           <div className="flex justify-between items-center p-4 rounded-lg bg-gray-50">
             <p className="text-purple-600">
               <strong>Наши тренера дадут максимальную поддержку</strong> и
               заботу о вас
             </p>
-            <img src="icon1.png" alt="icon" className="w-10 h-10" />
+            <img src={features4} alt="icon" className="w-10 h-10" />
           </div>
           <div className="flex justify-between items-center p-4 rounded-lg bg-gray-50">
             <p>
               <strong>Мы подготовим персональную программу</strong> под ваши
               особенности и предпочтения
             </p>
-            <img src="icon1.png" alt="icon" className="w-10 h-10" />
+            <img src={features5} alt="icon" className="w-10 h-10" />
           </div>
         </div>
       </section>
@@ -95,46 +132,40 @@ const App = () => {
         <h2 className="text-xl font-bold text-gray-900 mb-4">
           Отзывы наших клиентов:
         </h2>
-        <Swiper spaceBetween={10} slidesPerView={1} className="w-full">
-          <SwiperSlide>
+        <Slider {...settings} className="w-full">
+          <div>
             <img
-              src="review1.png"
+              src={Testimonial1}
               alt="Review 1"
               className="w-full rounded-lg"
             />
-          </SwiperSlide>
-          <SwiperSlide>
+          </div>
+          <div>
             <img
-              src="review2.png"
-              alt="Review 2"
+              src={Testimonial1}
+              alt="Review 1"
               className="w-full rounded-lg"
             />
-          </SwiperSlide>
-          <SwiperSlide>
+          </div>
+          <div>
             <img
-              src="review3.png"
-              alt="Review 3"
+              src={Testimonial1}
+              alt="Review 1"
               className="w-full rounded-lg"
             />
-          </SwiperSlide>
-          <SwiperSlide>
+          </div>
+          <div>
             <img
-              src="review4.png"
-              alt="Review 4"
+              src={Testimonial1}
+              alt="Review 1"
               className="w-full rounded-lg"
             />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="review5.png"
-              alt="Review 5"
-              className="w-full rounded-lg"
-            />
-          </SwiperSlide>
-        </Swiper>
+          </div>
+        </Slider>
       </section>
 
       <section className="mt-8 w-full max-w-3xl bg-white p-6 rounded-lg shadow-md text-center">
+        <img src={threeHundred} alt="threeHundred" />
         <h2 className="text-xl font-bold text-gray-900">
           Мы обладаем всеми возможными знаниями, которые идеально подойдут
           именно вам!
@@ -150,6 +181,9 @@ const App = () => {
           Бесплатную персональную программу тренировок
         </p>
       </section>
+      <footer>
+        <img src={footerBg} alt="footerBg" width={"700px"} />
+      </footer>
     </div>
   );
 };
