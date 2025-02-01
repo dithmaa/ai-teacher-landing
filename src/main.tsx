e09a6bac29b1831100ce0be937fea56df5665d0b
Baltukov Nomto <disma785@gmail.com>
2025-02-02 04:24:22 +0800

    Сделал сайт и поел морожку

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import Quiz from "./pages/quiz";
+import Quiz from "./pages/Quiz";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
