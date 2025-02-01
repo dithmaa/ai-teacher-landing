6e839c2df994477b975562fd69da299e1e4ec024
Baltukov Nomto <disma785@gmail.com>
2025-02-02 01:01:08 +0800

    d

--- src/App.tsx
+++ src/App.tsx
@@ -27,189 +27,197 @@ const settings = {
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center main-layout">
-      <main className="w-full">
-        <header className="w-full">
-          <div className="container max-w-[90%] mx-auto my-[50px]">
-            <img src={Logo} alt="FitMentor Logo" className="w-60 mb-4" />
-          </div>
-        </header>
-        <div className="container max-w-[90%] mx-auto">
-          <div className="main-content w-full flex">
-            <div className="xl:max-w-[630px] lg:max-w-[500px] md:max-w-[370px] max-w-full">
-              <h1 className="text-[45px] lg:text-[60px] xl:text-[75px] font-medium text-gray-900 mb-6 leading-13 lg:leading-19">
-                Приведи себя <br /> в лучшую форму своей жизни!
-              </h1>
-              <p className="text-[#000] mt-2 text-[18px] lg:text-[20px] xl:text-[23px] my-3 font-[400]">
-                Преобразуйте фитнес и питание через персональные
-                онлайн-тренировки: чаты с тренером, живые видео и приложение для
-                ваших целей.
-              </p>
-              <button className="cursor-pointer transform lg:translate-x-[120px] mt-0 px-15 py-6 bg-purple-600 text-white rounded-full shadow-lg hover:bg-purple-700 font-[500] text-[26px] lg:text-[35px]">
-                Записаться
-              </button>
+    <div>
+      <main className="w-full bg-none md:bg-[url('./assets/main-bg.png')] md:bg-contain md:bg-no-repeat md:bg-[right_70px] 2xl:bg-[url('./assets/main-bg-full.png')]">
+        <div className="main-layout">
+          <header className="w-full">
+            <div className="container max-w-[90%] mx-auto my-[50px]">
+              <img src={Logo} alt="FitMentor Logo" className="w-60 mb-4" />
+            </div>
+          </header>
+          <div className="container max-w-[90%] mx-auto">
+            <div className="main-content w-full flex">
+              <div className="xl:max-w-[630px] lg:max-w-[500px] md:max-w-[370px] max-w-full">
+                <h1 className="text-[45px] lg:text-[60px] xl:text-[75px] font-medium text-gray-900 mb-6 leading-13 lg:leading-19">
+                  Приведи себя <br /> в лучшую форму своей жизни!
+                </h1>
+                <p className="text-[#000] mt-2 text-[18px] lg:text-[20px] xl:text-[23px] my-3 font-[400]">
+                  Преобразуйте фитнес и питание через персональные
+                  онлайн-тренировки: чаты с тренером, живые видео и приложение
+                  для ваших целей.
+                </p>
+                <button className="cursor-pointer transform lg:translate-x-[120px] mt-0 px-15 py-6 bg-purple-600 text-white rounded-full shadow-lg hover:bg-purple-700 font-[500] text-[26px] lg:text-[35px]">
+                  Записаться
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </main>
+      <div className="min-h-screen flex flex-col items-center main-layout">
+        <div className="container max-w-[90%] mx-auto">
+          <section className="py-10 mt-10 lg:mt-10 bg-white rounded-[50px] w-[70%] mx-auto">
+            <div className="flex flex-col lg:flex-row items-center justify-center">
+              <img src={rubbleIcon} className="w-20 mr-5" alt="" />
+              <h2 className="text-xl font-bold flex flex-col text-center">
+                <span className="text-[25px]">
+                  {" "}
+                  Месяц тренировок с тренером{" "}
+                </span>
+                <span className="text-gray-900 text-[50px]">
+                  по цене одной!
+                </span>
+              </h2>
+            </div>
+          </section>
+          <section className="mt-8 w-full">
+            <h3 className="text-[50px] font-semibold text-gray-900 mt-6 text-center leading-16">
+              Как вы будете работать <br /> со своим тренером:
+            </h3>
 
-      <div className="container max-w-[90%] mx-auto">
-        <section className="py-10 mt-10 lg:mt-10 bg-white rounded-[50px] w-[70%] mx-auto">
-          <div className="flex flex-col lg:flex-row items-center justify-center">
-            <img src={rubbleIcon} className="w-20 mr-5" alt="" />
-            <h2 className="text-xl font-bold flex flex-col text-center">
-              <span className="text-[25px]"> Месяц тренировок с тренером </span>
-              <span className="text-gray-900 text-[50px]">по цене одной!</span>
+            <div className="flex gap-9 mt-20 flex-wrap">
+              <div className="flex items-center flex-col bg-white rounded-[5%] md:rounded-[10%] lg:rounded-[30%] border-2 border-[#e4e4e4] px-7 pb-10 text-center w-[100%] md:w-[47%] lg:w-[21%]">
+                <div className="flex items-center transform translate-y-[-45%]">
+                  <span className="text-[40px] lg:text-[45px] mr-[5px]">1</span>
+                  <img src={step1} alt="" />
+                </div>
+                <p>Пройдите тест и подберите идеального тренера</p>
+              </div>
+              <div className="flex items-center flex-col bg-white rounded-[5%] md:rounded-[10%] lg:rounded-[30%] border-2 border-[#e4e4e4] px-7 pb-10 text-center w-[100%] md:w-[47%] lg:w-[21%]">
+                <div className="flex items-center transform translate-y-[-45%]">
+                  <span className="text-[40px] lg:text-[45px] mr-[5px]">2</span>
+                  <img src={step2} alt="" />
+                </div>
+                <p>
+                  Видеозвонок для согласования индивидуального плана упражнений
+                  и питания
+                </p>
+              </div>
+              <div className="flex items-center flex-col bg-white rounded-[5%] md:rounded-[10%] lg:rounded-[30%] border-2 border-[#e4e4e4] px-7 pb-10 text-center w-[100%] md:w-[47%] lg:w-[21%]">
+                <div className="flex items-center transform translate-y-[-45%]">
+                  <span className="text-[40px] lg:text-[45px] mr-[5px]">3</span>
+                  <img src={step3} alt="" />
+                </div>
+                <p>Выполняйте тренировки, разработанные специально для вас</p>
+              </div>
+              <div className="flex items-center flex-col bg-white rounded-[5%] md:rounded-[10%] lg:rounded-[30%] border-2 border-[#e4e4e4] px-7 pb-10 text-center w-[100%] md:w-[47%] lg:w-[21%]">
+                <div className="flex items-center transform translate-y-[-45%]">
+                  <span className="text-[40px] lg:text-[45px] mr-[5px]">4</span>
+                  <img src={step4} alt="" />
+                </div>
+                <p>Будьте очень мотивированы в время еженедельных проверок</p>
+              </div>
+            </div>
+          </section>
+
+          <section className="mt-8 w-fullp-6">
+            <div className="grid grid-cols-1 gap-4">
+              <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
+                <p className="text-[30px]">
+                  <strong>Подберем проверенного тренера</strong> на базе вашего
+                  запроса
+                </p>
+                <img src={features1} alt="icon" className="w-[160px]" />
+              </div>
+              <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
+                <p className="text-purple-600 text-[30px]">
+                  <strong>Гибкое расписание.</strong> Наши тренера работают в
+                  онлайн режиме 24/7
+                </p>
+                <img src={features2} alt="icon" className="w-[160px]" />
+              </div>
+              <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
+                <p className="text-[30px]">
+                  <strong>Высокая вовлеченность тренера</strong> и контроль
+                  техники.
+                </p>
+                <img src={features3} alt="icon" className="w-[160px]" />
+              </div>
+              <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
+                <p className="text-purple-600 text-[30px]">
+                  <strong>Наши тренера дадут максимальную поддержку</strong> и
+                  заботу о вас
+                </p>
+                <img src={features4} alt="icon" className="w-[160px]" />
+              </div>
+              <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
+                <p className="text-[30px]">
+                  <strong>Мы подготовим персональную программу</strong> под ваши
+                  особенности и предпочтения
+                </p>
+                <img src={features5} alt="icon" className="w-[160px]" />
+              </div>
+            </div>
+          </section>
+
+          <section className="mt-8 w-full">
+            <h2 className="text-[50px] font-[500] text-gray-900 mb-4 text-center">
+              Отзывы наших клиентов:
             </h2>
-          </div>
-        </section>
-        <section className="mt-8 w-full">
-          <h3 className="text-[50px] font-semibold text-gray-900 mt-6 text-center leading-16">
-            Как вы будете работать <br /> со своим тренером:
-          </h3>
+            <div className="mx-auto">
+              <Slider {...settings} className="w-full mx-auto">
+                <div>
+                  <img
+                    src={Testimonial1}
+                    alt="Review 1"
+                    className="w-[600px] rounded-lg mx-auto"
+                  />
+                </div>
+                <div>
+                  <img
+                    src={Testimonial1}
+                    alt="Review 1"
+                    className="w-[600px] rounded-lg mx-auto"
+                  />
+                </div>
+                <div>
+                  <img
+                    src={Testimonial1}
+                    alt="Review 1"
+                    className="w-[600px] rounded-lg mx-auto"
+                  />
+                </div>
+                <div>
+                  <img
+                    src={Testimonial1}
+                    alt="Review 1"
+                    className="w-[600px] rounded-lg mx-auto"
+                  />
+                </div>
+              </Slider>
+            </div>
+          </section>
 
-          <div className="flex gap-9 mt-20 flex-wrap">
-            <div className="flex items-center flex-col bg-white rounded-[5%] md:rounded-[10%] lg:rounded-[30%] border-2 border-[#e4e4e4] px-7 pb-10 text-center w-[100%] md:w-[47%] lg:w-[21%]">
-              <div className="flex items-center transform translate-y-[-45%]">
-                <span className="text-[40px] lg:text-[45px] mr-[5px]">1</span>
-                <img src={step1} alt="" />
-              </div>
-              <p>Пройдите тест и подберите идеального тренера</p>
-            </div>
-            <div className="flex items-center flex-col bg-white rounded-[5%] md:rounded-[10%] lg:rounded-[30%] border-2 border-[#e4e4e4] px-7 pb-10 text-center w-[100%] md:w-[47%] lg:w-[21%]">
-              <div className="flex items-center transform translate-y-[-45%]">
-                <span className="text-[40px] lg:text-[45px] mr-[5px]">2</span>
-                <img src={step2} alt="" />
-              </div>
-              <p>
-                Видеозвонок для согласования индивидуального плана упражнений и
-                питания
-              </p>
-            </div>
-            <div className="flex items-center flex-col bg-white rounded-[5%] md:rounded-[10%] lg:rounded-[30%] border-2 border-[#e4e4e4] px-7 pb-10 text-center w-[100%] md:w-[47%] lg:w-[21%]">
-              <div className="flex items-center transform translate-y-[-45%]">
-                <span className="text-[40px] lg:text-[45px] mr-[5px]">3</span>
-                <img src={step3} alt="" />
-              </div>
-              <p>Выполняйте тренировки, разработанные специально для вас</p>
-            </div>
-            <div className="flex items-center flex-col bg-white rounded-[5%] md:rounded-[10%] lg:rounded-[30%] border-2 border-[#e4e4e4] px-7 pb-10 text-center w-[100%] md:w-[47%] lg:w-[21%]">
-              <div className="flex items-center transform translate-y-[-45%]">
-                <span className="text-[40px] lg:text-[45px] mr-[5px]">4</span>
-                <img src={step4} alt="" />
-              </div>
-              <p>Будьте очень мотивированы в время еженедельных проверок</p>
-            </div>
-          </div>
-        </section>
-
-        <section className="mt-8 w-fullp-6">
-          <div className="grid grid-cols-1 gap-4">
-            <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
-              <p className="text-[30px]">
-                <strong>Подберем проверенного тренера</strong> на базе вашего
-                запроса
-              </p>
-              <img src={features1} alt="icon" className="w-[160px]" />
-            </div>
-            <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
-              <p className="text-purple-600 text-[30px]">
-                <strong>Гибкое расписание.</strong> Наши тренера работают в
-                онлайн режиме 24/7
-              </p>
-              <img src={features2} alt="icon" className="w-[160px]" />
-            </div>
-            <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
-              <p className="text-[30px]">
-                <strong>Высокая вовлеченность тренера</strong> и контроль
-                техники.
-              </p>
-              <img src={features3} alt="icon" className="w-[160px]" />
-            </div>
-            <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
-              <p className="text-purple-600 text-[30px]">
-                <strong>Наши тренера дадут максимальную поддержку</strong> и
-                заботу о вас
-              </p>
-              <img src={features4} alt="icon" className="w-[160px]" />
-            </div>
-            <div className="flex justify-between items-center p-4 rounded-[60px] py-2 px-5 bg-white border-1 border-[#e4e4e4]">
-              <p className="text-[30px]">
-                <strong>Мы подготовим персональную программу</strong> под ваши
-                особенности и предпочтения
-              </p>
-              <img src={features5} alt="icon" className="w-[160px]" />
-            </div>
-          </div>
-        </section>
-
-        <section className="mt-8 w-full">
-          <h2 className="text-[50px] font-[500] text-gray-900 mb-4 text-center">
-            Отзывы наших клиентов:
-          </h2>
-          <div className="mx-auto">
-            <Slider {...settings} className="w-full mx-auto">
-              <div>
-                <img
-                  src={Testimonial1}
-                  alt="Review 1"
-                  className="w-[600px] rounded-lg mx-auto"
-                />
-              </div>
-              <div>
-                <img
-                  src={Testimonial1}
-                  alt="Review 1"
-                  className="w-[600px] rounded-lg mx-auto"
-                />
-              </div>
-              <div>
-                <img
-                  src={Testimonial1}
-                  alt="Review 1"
-                  className="w-[600px] rounded-lg mx-auto"
-                />
-              </div>
-              <div>
-                <img
-                  src={Testimonial1}
-                  alt="Review 1"
-                  className="w-[600px] rounded-lg mx-auto"
-                />
-              </div>
-            </Slider>
-          </div>
-        </section>
-
-        <section className="mt-8 w-full p-6 text-center">
-          <img
-            src={threeHundred}
-            alt="threeHundred"
-            className="mx-auto max-w-[900px] w-full"
-          />
-          <h2 className="text-3xl font-[500] text-gray-900">
-            Мы обладаем всеми возможными знаниями, которые идеально подойдут
-            именно вам!
-          </h2>
-          <p className="text-gray-700 mt-2 text-3xl">
-            От бодибилдинга до послеродового периода, гормональных и медицинских
-            проблем.
-          </p>
-          <button className="cursor-pointer mt-10 mb-3 px-15 py-6 bg-purple-600 text-white rounded-full shadow-lg hover:bg-purple-700 font-[500] text-[35px]">
-            Получить
-          </button>
-          <p className="text-gray-500 mt-2 text-2xl">
-            Бесплатную персональную программу тренировок
-          </p>
-        </section>
-        <footer className="text-center">
-          <img
-            className="mx-auto"
-            src={footerBg}
-            alt="footerBg"
-            width={"900px"}
-          />
-        </footer>
+          <section className="mt-8 w-full p-6 text-center">
+            <img
+              src={threeHundred}
+              alt="threeHundred"
+              className="mx-auto max-w-[900px] w-full"
+            />
+            <h2 className="text-3xl font-[500] text-gray-900">
+              Мы обладаем всеми возможными знаниями, которые идеально подойдут
+              именно вам!
+            </h2>
+            <p className="text-gray-700 mt-2 text-3xl">
+              От бодибилдинга до послеродового периода, гормональных и
+              медицинских проблем.
+            </p>
+            <button className="cursor-pointer mt-10 mb-3 px-15 py-6 bg-purple-600 text-white rounded-full shadow-lg hover:bg-purple-700 font-[500] text-[35px]">
+              Получить
+            </button>
+            <p className="text-gray-500 mt-2 text-2xl">
+              Бесплатную персональную программу тренировок
+            </p>
+          </section>
+          <footer className="text-center">
+            <img
+              className="mx-auto"
+              src={footerBg}
+              alt="footerBg"
+              width={"900px"}
+            />
+          </footer>
+        </div>
       </div>
     </div>
   );
